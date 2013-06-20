c6d8076ed7df43ce825472fbda95059098d4c627
Javier Santacruz <javier.santacruz.lc@gmail.com>
2013-06-20 20:28:26 +0200

    Adds setup.py and packaging info

---
+++ setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='ipodio',
+    version='0.0.1',
+    description='iPod command line managing tool',
+    author='Javier Santacruz',
+    author_email='javier.santacruz.lc@gmail.com',
+    url='https://github.com/jvrsantacruz/ipodio',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Environment :: Console',
+        'Operating System :: POSIX',
+        'Development Status :: 3 - Alpha',
+        'Topic :: Multimedia :: Sound/Audio',
+        'Intended Audience :: End Users/Desktop',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
+    platforms=['Unix'],
+)
