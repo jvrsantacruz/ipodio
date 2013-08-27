998537dbc4ff9ef2803d2d0d87e65805e2a2d2cd
Javier Santacruz <javier.santacruz.lc@gmail.com>
2013-08-27 17:06:43 +0200

    Moves Console class to its own module

--- ipodio/cli.py
+++ ipodio/cli.py
@@ -16,9 +16,10 @@ import os
 import sys
 import shutil
 
-import ipodio
-
 from docopt import docopt
+
+import ipodio
+from .console import Console
 
 
 def first(collection):
@@ -42,44 +43,6 @@ def _sorted_tracks(tracks, key=None):
     tracks.sort(key=by('artist'))
 
     return tracks
-
-
-class Console(object):
-    _width = None
-
-    def _get_console_size(self):
-        import sys
-        return (self._get_window_size(sys.stdin)
-                or self._get_window_size(sys.stdout)
-                or self._get_window_size(sys.stderr)
-                or self._get_term_window_size()
-                or (25, 80))
-
-    def _get_term_window_size(self):
-        try:
-            with os.open(os.ctermid(), os.O_RDONLY) as term:
-                return self._get_window_size(term)
-        except:
-            return None
-
-    def _get_window_size(self, fd):
-        import struct, fcntl, termios
-        try:
-            data = fcntl.ioctl(fd, termios.TIOCGWINSZ, '12345678')
-            height, width, hp, wp = struct.unpack('HHHH', data)
-            return height, width
-        except:
-            return None
-
-    @property
-    def width(self):
-        if self._width is None:
-            _, self._width = self._get_console_size()
-        return self._width
-
-    def relative_width(self, percentage):
-        """Return the width of a part of the screen in number of characters"""
-        return int((self.width - 4) * percentage)
 
 
 def _separator(sep, console=Console()):
---
+++ ipodio/console.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import fcntl
+import struct
+import termios
+
+
+class Console(object):
+    _width = None
+
+    def _get_console_size(self):
+        return (self._get_window_size(sys.stdin)
+                or self._get_window_size(sys.stdout)
+                or self._get_window_size(sys.stderr)
+                or self._get_term_window_size()
+                or (25, 80))
+
+    def _get_term_window_size(self):
+        try:
+            with os.open(os.ctermid(), os.O_RDONLY) as term:
+                return self._get_window_size(term)
+        except:
+            return None
+
+    def _get_window_size(self, fd):
+        try:
+            data = fcntl.ioctl(fd, termios.TIOCGWINSZ, '12345678')
+            height, width, hp, wp = struct.unpack('HHHH', data)
+            return height, width
+        except:
+            return None
+
+    @property
+    def width(self):
+        if self._width is None:
+            _, self._width = self._get_console_size()
+        return self._width
+
+    def relative_width(self, percentage):
+        """Return the width of a part of the screen in number of characters"""
+        return int((self.width - 4) * percentage)
