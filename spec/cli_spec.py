edbfdebecd91cdb0654c8d1f1609b8cdbb5e5fba
Javier Santacruz <javier.santacruz.lc@gmail.com>
2013-08-30 14:01:31 +0200

    Adds further interface testing

--- spec/cli_spec.py
+++ spec/cli_spec.py
@@ -42,6 +42,17 @@ with describe('ipodio') as _:
                 expect(stdout_lines).to.have.length(
                     length_of_header + number_of_songs + length_of_footer)
 
+            def it_should_print_a_line_per_song_when_filtering_using_expression():
+                execution = _.env.run(*_.cmd + ['list'] + _.expression.split())
+
+                length_of_footer = 2
+                length_of_header = 1
+                number_of_songs = 1
+                stdout_lines = execution.stdout.split('\n')
+
+                expect(stdout_lines).to.have.length(
+                    length_of_header + number_of_songs + length_of_footer)
+
             @before.each
             def setup_list():
                 _populate_ipod(_.mountpoint_path, _.songs)
@@ -57,8 +68,22 @@ with describe('ipodio') as _:
 
             expect(execution.stdout.count('Sending')).to.be(len(_.songs))
 
+        def it_should_find_files_within_a_directory():
+            execution = _.env.run(*_.cmd + ['push', _.fixtures_path])
+
+            expect(execution.stdout.count('Sending')).to.be(len(_.songs))
+
+        def it_should_copy_song_files_into_the_ipod():
+            execution = _.env.run(*_.cmd + ['--force', 'push'] + _.song_paths)
+
+            number_of_added_songs = sum(1 for path in execution.files_created
+                if 'iPod_Control/Music' in path and path.endswith('mp3'))
+
+            expect(number_of_added_songs).to.be(2)
+
         def it_should_refuse_to_duplicate_a_song():
-            _.env.run(*_.cmd + ['push'] + _.song_paths)
+            _populate_ipod(_.mountpoint_path, _.songs)
+
             execution = _.env.run(*_.cmd + ['push'] + _.song_paths)
 
             expect(execution.stdout.count('Not sending')).to.be(len(_.songs))
@@ -71,6 +96,12 @@ with describe('ipodio') as _:
             expect(execution.stdout.count('Sending')).to.be(len(_.songs))
 
     with context('when executing rename'):
+        def it_should_return_an_error_when_rename_with_bad_expressions():
+            execution = _.env.run(
+                *_.cmd + ['rm', _.bad_expression, _.bad_expression], expect_error=True)
+
+            expect(execution.stdout).to.have("Error: Invalid expression")
+
         def it_should_show_an_error_when_called_without_arguments():
             execution = _.env.run(*_.cmd + ['rename'], expect_error=True)
 
@@ -80,6 +111,22 @@ with describe('ipodio') as _:
             execution = _.env.run(*_.cmd + ['rename', _.expression], expect_error=True)
 
             expect(execution.stderr).to.have('Usage')
+
+    with context('when executing rm'):
+        def it_should_return_an_error_when_called_without_arguments():
+            execution = _.env.run(*_.cmd + ['rm'], expect_error=True)
+
+            expect(execution.stderr).to.have("Usage:")
+
+        def it_should_return_an_error_when_rm_with_bad_expressions():
+            execution = _.env.run(*_.cmd + ['rm', _.bad_expression], expect_error=True)
+
+            expect(execution.stdout).to.have("Error: Invalid expression")
+
+        def it_should_print_message_when_removing_no_songs():
+            execution = _.env.run(*_.cmd + ['rm', 'foobarbaztaz'], expect_error=True)
+
+            expect(execution.stdout).to.have("No tracks removed.")
 
     @before.each
     def setup():
@@ -95,10 +142,13 @@ with describe('ipodio') as _:
     @before.all
     def fixture():
         _.bad_expression = '?'
-        _.expression = 'john lennon'
+        _.expression = 'richard stall'
 
         _.env_path = 'testing-sandbox'
         _.env = TestFileEnvironment(_.env_path)
+
+        _.fixtures = 'fixtures'
+        _.fixtures_path = '../fixtures'
 
         _.songs = ['fixtures/song1.mp3', 'fixtures/song2.mp3']
         _.song_paths = [os.path.join('..', song) for song in _.songs]
